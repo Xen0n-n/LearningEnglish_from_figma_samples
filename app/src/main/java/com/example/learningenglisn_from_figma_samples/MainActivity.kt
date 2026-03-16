623aa2c2071009ce29814a54b4da44f2d06acec0
Xenon <nsoft13@gmail.com>
2026-03-16 22:42:59 +0300

    Added Simple interaction with true answer

--- app/src/main/java/com/example/learningenglisn_from_figma_samples/MainActivity.kt
+++ app/src/main/java/com/example/learningenglisn_from_figma_samples/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.example.learningenglisn_from_figma_samples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,35 +19,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) // сохраняем экземпляр привязки, layoutInflater связывает разметку с activity
-        enableEdgeToEdge()
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-
-//      Есть два способа подтянуть ID элементов
-        // findViewById
-        // ViewBinding
-
-        // findViewById:
-//        setContentView(R.layout.activity_main)
-//        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
-//        tvQuestionWord.text = "Dream"
-//        tvQuestionWord.setTextColor(Color.BLUE)
-//
-//        // НАИБОЛЕЕ ПРЕДПОЧТИТЕЛЬНО
-//        // цвет buttonBlueColor лежит в values/colors.xml
-//        tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.buttonBlueColor))
-
-        //ViewBinding
-        //Включить зависимость в кфг градла build.gradle.kts
         setContentView(binding.root)
+        // neutral
+        // correct
+        // incorrect
+        binding.layoutAnswer3.setOnClickListener {
+            markAnswerCorrect()
+        }
+    }
 
-
-        binding.tvQuestionWord.text = "Dream"
-        binding.tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.buttonBlueColor))
-
+    private fun markAnswerCorrect() {
+        binding.layoutAnswer3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_correct
+        )
+        binding.btnSkip.isVisible = false
+        binding.layoutCorrectAnswerContinue.isVisible = true
     }
 }
